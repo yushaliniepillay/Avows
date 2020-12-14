5daa12a09b8039a3ce536bad096a88dfef5c85e2
yushaliniepillay <yushalinie97@hotmail.com>
2020-12-14 20:36:03 +0800

    add slide screen

---
+++ src/component/home/SliderScreen.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+import Slider from "react-animated-slider";
+import "react-animated-slider/build/horizontal.css";
+import "normalize.css/normalize.css";
+import "../home/slider-animations.css";
+
+class SliderScreen extends React.Component {
+
+    constructor() {
+        super()
+        this.state = {
+            sliders: []
+        }
+    }
+
+    componentWillMount() {
+        fetch(`http://localhost:1337/sliders`).then((response) => {
+            if (response.status >= 400) {
+                throw new Error("Bad Response From Server");
+            }
+            return response.json();
+        }).then((sliders) => {
+            this.setState({ sliders: sliders });
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <Slider className="slider-wrapper" autoplay={3000}>
+                    {
+                        this.state.sliders.map(({ id, title, description, list, image }) => (
+                            <div
+                                key={id}
+                                className="slider-content"
+                                style={{ background: <img src={`http://localhost:1337${image.url}`} /> }} >
+                                <div className="inner">
+                                    <h1>{title}</h1>
+                                    <p>{description}</p>
+                                    <p>{list}</p>
+                                    {/* <button>{item.button}</button> */}
+                                </div>
+                                <div className="slider-button">
+                                    <section>
+                                        <Button class="active" variant="danger" size="lg" href=''>View Details</Button>
+                                    </section>
+                                </div>
+                            </div>
+                        ))
+                    }
+                </Slider>
+            </div >
+
+        )
+
+    }
+
+}
+export default SliderScreen;
--- src/component/home/homePage.js
+++ src/component/home/homePage.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import fetch from 'isomorphic-fetch';
+import Sliders from './SliderScreen';
 import HomeListItem from './homelistitem';
 import { Button } from 'react-bootstrap';
-
 
 class HomePage extends React.Component {
 
@@ -28,60 +28,15 @@ class HomePage extends React.Component {
         return (
 
             <div>
-                <div class="owl-carousel owl-theme">
-                    <div class="item">
-                        <img src="https://cdn.pixabay.com/photo/2016/03/09/09/22/workplace-1245776_960_720.jpg" alt="images not found" >
-                            <div class="cover">
-                                <div class="container">
-                                    <div class="header-content">
-                                        <div class="line"></div>
-                                        <h2>Teimagine Digital Experience with</h2>
-                                        <h1>Start-ups and solutions</h1>
-                                        <h4>We help entrepreneurs, start-ups and enterprises shape their ideas into products</h4>
-                                    </div>
-                                </div>
-                            </div>
-                        </img>
-                    </div>
+               <Sliders />
 
-                    <div class="item">
-                        <img src="https://cdn.pixabay.com/photo/2019/11/18/02/41/autumn-leaves-4633854_960_720.jpg" alt="images not found">
-                            <div class="cover">
-                                <div class="container">
-                                    <div class="header-content">
-                                        <div class="line animated bounceInLeft"></div>
-                                        <h2>Reimagine Digital Experience with</h2>
-                                        <h1>Intelligent solutions</h1>
-                                        <h4>We help entrepreneurs, start-ups and enterprises shape their ideas into products</h4>
-                                    </div>
-                                </div>
-                            </div>
-                        </img>
-                    </div>
+                <div className="button-mv">
+                    <Button class="active" variant="danger" size="lg">Our Mission</Button>
+                    <Button variant="light" size="lg">Our Vission</Button>
+                </div>
+                <h1 className="mt-5">React Testing Application Website</h1>
 
-                    <div class="item">
-                        <img src="https://cdn.pixabay.com/photo/2020/04/16/02/44/dandelion-5048825_960_720.jpg" alt="images not found">
-                            <div class="cover">
-                                <div class="container">
-                                    <div class="header-content">
-                                        <div class="line animated bounceInLeft"></div>
-                                        <h2>Peimagine Digital Experience with</h2>
-                                        <h1>Intelligent Solutions</h1>
-                                        <h4>We help entrepreneurs, start-ups and enterprises shape their ideas into products</h4>
-                                    </div>
-                                </div>
-                            </div>
-                        </img>
-                    </div>
-
-                    <div className="button-mv">
-                        <Button class="active" variant="danger" size="lg">Our Mission</Button>
-                        <Button variant="light" size="lg">Our Vission</Button>
-                    </div>
-                    <h1 className="mt-5">React Testing Application Website</h1>
-
-                </div >
-            </div>
+            </div >
 
             // <div>
             //     {
